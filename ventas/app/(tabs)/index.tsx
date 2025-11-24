e4a3d660d29df0a061b5195e067fe0b1f33f3344
Julianrvillamil <julianrvillamil@gmail.com>
2025-11-23 23:48:27 -0500

    fix: comentar el componente APKDebugger en la pantalla de clientes

--- ventas/app/(tabs)/index.tsx
+++ ventas/app/(tabs)/index.tsx
@@ -293,7 +293,7 @@ export default function ClientesScreen() {
         />
       </View>
        {/* APK Debugger */}
-      <APKDebugger />
+      {/* <APKDebugger /> */}
     </SafeAreaView>
   );
 }
